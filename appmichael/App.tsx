9994a178e994004fdbc71780c4791305b43a853e
JemmEdu16 <159553412+MichaelMMDev@users.noreply.github.com>
2024-05-03 11:53:34 -0300

    u
    hs

--- appmichael/App.tsx
+++ appmichael/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { View, Text} from 'react-native';
+import { SignIn } from './src/screens';
 
 export default function app() {
   return(
-    <View>
-      <Text>olá mundoooo</Text>
-    </View>
+    <SignIn/>
   )
 }
