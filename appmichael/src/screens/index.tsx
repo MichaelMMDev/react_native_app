52cca80ce1d4c98d59b3efed1c3f2ef5bea7ab5f
lilicahippie <159553412+MichaelMMDev@users.noreply.github.com>
2024-05-17 11:57:19 -0300

    j
    k

--- appmichael/src/screens/index.tsx
+++ appmichael/src/screens/index.tsx
@@ -5,17 +5,17 @@ import { styles } from './style';
 
 
 export  function SignIn() {
-  const [text, setText] = useState ("");
+  const [text, setText] = useState ("Você ainda não digitou nada");
 
   return(
     <View style= {styles.container}>
       <Text>
         olá mundoooo
       </Text>
-      <TextInput style={styles.input}/>
+      <TextInput style={styles.input} onChangeText={setText}/>
 
      <Text>
-      voce digitou:
+      voce digitou: {text}
      </Text>
 
     </View>
