a6efc9291edcb8a5124afcd2eec9fc0c37a249e1
lilicahippie <159553412+MichaelMMDev@users.noreply.github.com>
2024-05-31 11:58:12 -0300

    HGSHGSJSHSH
    JSSJSJJX

--- appmichael/src/screens/index.tsx
+++ appmichael/src/screens/index.tsx
@@ -1,16 +1,29 @@
 import React, {useState} from 'react';
-import { View, Text,TextInput,Image} from 'react-native';
+import { View, Text,TextInput,Image, StatusBar} from 'react-native';
 import { styles } from './style';
-import IlustrationImg from '../../assets/ilustration.png';
+import IlustrationImg from '../assets/illustration.png';
 
 
 export  function SignIn() {
  
   return(
    <View style={styles.container}>
+    <StatusBar barStyle='light-content' backgroundColor={"trasparent"} translucent /> 
+     
+      <Image source={IlustrationImg} style={styles.image} resizeMode='stretch'/>
+   <View style={styles.container}>
 
-     <Image source={IlustrationImg} style={styles.image} resizeMode='stretch'/>
+     <Text style={styles.title}>
+      Organize Suas {'\n'}
+      Jogatinas{'\n'}
+      Facilmente{'\n'}
+     </Text>
+     <Text style={styles.subtitle}>
+      Crie grupos para jogar seus games{'\n'}
+      Favoritos com seus amigos
+     </Text>
 
+   </View>
    </View>
   )
 }
